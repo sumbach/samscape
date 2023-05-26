60dcb552430b52f63798a5e82447da37af0fa32e
Sam Umbach <samumbach@gmail.com>
2023-05-25 22:45:35 -0400

    basic HTTP request

--- src/samscape/core.clj
+++ src/samscape/core.clj
@@ -1,1 +1,55 @@
-(ns samscape.core)
+(ns samscape.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string])
+  (:import (java.net Socket)
+           (java.io FilterInputStream FilterOutputStream)))
+
+(defn parse [s]
+  (assert (string/starts-with? s "http://"))
+  (let [x (subs s (.length "http://"))
+        [host path] (string/split x #"/" 2)
+        path (str "/" path)]
+    {:host host :path path}))
+
+(defn request-payload [url]
+  (format (str "GET %s HTTP/1.0\r\n"
+               "Host: %s\r\n"
+               "\r\n")
+          (:path url)
+          (:host url)))
+
+(defn read-headers [r]
+  (loop [headers []]
+    (let [line (.readLine r)]
+      (if (= "" line)
+        headers
+        (let [[header value] (string/split line #":" 2)
+              value (string/trim value)]
+          (recur (conj headers [header value])))))))
+
+(defn os [socket]
+  (proxy [FilterOutputStream] [(.getOutputStream socket)]
+    (close []
+      (.shutdownOutput socket))))
+
+(defn is [socket]
+  (proxy [FilterInputStream] [(.getInputStream socket)]
+    (close []
+      #_(.shutdownInput socket))))
+
+(defn request [s]
+  (let [url (parse s)
+        host (:host url)
+        port 80
+        [host port] ["localhost" 22222] ;; TODO
+        payload (request-payload url)]
+    (with-open [socket (Socket. host port)]
+      (with-open [o (os socket)]
+        (.write o (.getBytes payload "UTF-8"))
+        (.flush o)) ;; TODO: is this necessary?
+      (with-open [i (is socket)
+                  r (io/reader i)]
+        (let [status-line (.readLine r)
+              [version status explanation] (string/split status-line #" " 3)
+              headers (read-headers r)]
+          [[version status explanation] headers])))))
--- test/samscape/core_test.clj
+++ test/samscape/core_test.clj
@@ -1,6 +1,31 @@
 (ns samscape.core-test
-  (:require [clojure.test :refer [deftest is]]
-            #_[samscape.core :as sut]))
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer [deftest is]]
+            [samscape.core :as sut])
+  (:import (java.net ServerSocket)))
 
-(deftest foo-test
-  (is (= 1 2)))
+(deftest parse-test
+  (is (thrown? java.lang.AssertionError (sut/parse "foo")))
+  (is (thrown? java.lang.AssertionError (sut/parse "http:")))
+  (is (thrown? java.lang.AssertionError (sut/parse "http:/")))
+  (is (= {:host ""            :path "/"} (sut/parse "http://")))
+  (is (= {:host "example.com" :path "/"} (sut/parse "http://example.com")))
+  (is (= {:host "example.com" :path "/"} (sut/parse "http://example.com/")))
+  (is (= {:host "example.com" :path "/foo/bar"} (sut/parse "http://example.com/foo/bar")))
+  (is (= {:host "example.com" :path "/foo/bar/"} (sut/parse "http://example.com/foo/bar/"))))
+
+(deftest request-test
+  (with-open [socket (ServerSocket. 22222)]
+    (let [f-server (future (.accept socket))
+          f-client (future (sut/request "http://example.com/foo/bar"))]
+      (try
+        (is (not= ::TIMEOUT (deref f-server 500 ::TIMEOUT)))
+        (with-open [i (sut/is @f-server)
+                    o (sut/os @f-server)]
+          (is (= "GET /foo/bar HTTP/1.0\r\nHost: example.com\r\n\r\n" (slurp (io/reader i))))
+          (.write o (.getBytes "HTTP/1.0 200 OK\r\nfoo:bar\r\nbaz:  z  \r\n\r\n" "UTF-8")))
+        (is (not= ::TIMEOUT (deref f-client 500 ::TIMEOUT)))
+        (is (= [["HTTP/1.0" "200" "OK"] [["foo" "bar"] ["baz" "z"]]] @f-client))
+        (finally
+          (try (some-> f-client deref .close) (catch Exception _)) ;; TODO: more specific catch?
+          (some-> f-server deref .close))))))
