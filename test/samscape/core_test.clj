b56139b8a49231f879fffb0c92b44dddb3a895b5
Sam Umbach <samumbach@gmail.com>
2023-05-26 10:50:23 -0400

    fixup! refactor tests

--- test/samscape/core_test.clj
+++ test/samscape/core_test.clj
@@ -16,17 +16,17 @@
 
 (deftest request-test
   (with-open [server-socket (ServerSocket. 22222)]
-    (let [f-client-socket (future (.accept server-socket))
+    (let [f-socket (future (.accept server-socket))
           f-client (future (sut/request "http://example.com/foo/bar"))]
       (try
-        (with-open [client-socket (deref f-client-socket 500 ::TIMEOUT)]
-          (is (not= ::TIMEOUT client-socket))
-          (with-open [i (sut/is client-socket)
-                      o (sut/os client-socket)]
+        (with-open [socket (deref f-socket 500 ::TIMEOUT)]
+          (is (not= ::TIMEOUT socket))
+          (with-open [i (sut/is socket)
+                      o (sut/os socket)]
             (is (= "GET /foo/bar HTTP/1.0\r\nHost: example.com\r\n\r\n" (slurp (io/reader i))))
             (.write o (.getBytes "HTTP/1.0 200 OK\r\nfoo:bar\r\nbaz:  z  \r\n\r\n" "UTF-8")))
           (let [res (deref f-client 500 ::TIMEOUT)]
             (is (not= ::TIMEOUT res))
             (is (= [["HTTP/1.0" "200" "OK"] [["foo" "bar"] ["baz" "z"]]] res))))
         (finally
-          (some-> f-client-socket (deref 0 nil) .close))))))
+          (some-> f-socket (deref 0 nil) .close))))))
